3144f347faa7a4a7a218a11dfbaf07a673b0088f
Anshydv18 <anshgithub@gmail.com>
2024-01-28 10:48:02 +0530

    changed

--- 01vite/src/App.jsx
+++ 01vite/src/App.jsx
@@ -1,11 +1,14 @@
 
 
-
+import Hello from "./Hello"
 
 function App() {
 
   return (
+    <>
+    <Hello/>
     <h1> Chai aur code with vite | Ansh Yadav</h1>
+    </>
   )
 }
 
---
+++ 01vite/src/Hello.jsx
@@ -0,0 +1,8 @@
+
+function Hello(){
+    return(
+        <h2>Hello guys  | welcome to react</h2>
+    )
+}
+
+export default Hello;
